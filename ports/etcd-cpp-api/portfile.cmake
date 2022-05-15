f63c4a6fac4f0431db6f1e1b4712ec0a30141351
yunlu <wylswz@163.com>
2022-05-15 10:30:24 +0800

    port etcd cpp api

---
+++ ports/etcd-cpp-api/portfile.cmake
@@ -0,0 +1,27 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO etcd-cpp-apiv3/etcd-cpp-apiv3
+    REF f21c45b362297131b191ea8fc5033f8ffca4cc04
+    SHA512 26ae637950b0d8d1527cce4f7a94aa0dce69598185c4e2040990e11ffdbe699b691be648b87537084c3503575baa65a52521659e160b27cce014c60406242d46
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DBUILD_ETCD_TESTS=OFF
+)
+set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
+set(VCPKG_POLICY_DLLS_WITHOUT_EXPORTS enabled)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/etcd-cpp-api)
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+vcpkg_copy_pdbs()
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/etcd-cpp-apiv3 RENAME copyright)
